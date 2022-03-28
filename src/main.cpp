743077f3a6862a1d6507e79f8ee03b84896df05a
Ruslan <heckfynook@gmail.com>
2022-03-28 13:27:35 +0300

    history tracking support for Manager

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,17 @@ int main(int* argc, char** argv)
 	m.change_position("Andre", "SEO Manager", "main");
 	auto e = m.find_employee("Andre", "main");
 	if (e) std::cout << e->salary;
-	m.save("some_data.xml");
+	//m.save("some_data.xml");
 
 	SERP::TManager tm;
 	tm.load("data.xml");
-	tm.save("new_data.xml");
+	tm.save_state();
+	tm.create_division("SEO");
+	tm.add_employee("SEO", { "Ivan", "Ivanovich", "Ivanov", "SEO manager", 10000 });
+	tm.save_state();
+	tm.undo();
+	tm.save("new_data2.xml");
+	tm.redo();
+	tm.save("new_data1.xml");
 	return 0;
 }
--- src/tmanager.cpp
+++ src/tmanager.cpp
@@ -3,11 +3,131 @@
 
 namespace SERP
 {
-	void TManager::load(const std::string& filename)
-	{
+    TManager::TManager(std::size_t state_capacity) : state_cap_(state_capacity)
+    {
+        cur_state_ = 0;
+        if (!state_cap_) return;
+    }
+
+    void TManager::add(const std::string& filename)
+    {
         pt::ptree tree;
-		pt::read_xml(filename, tree);
-        for (auto& dep : tree.get_child("departments"))
+        pt::read_xml(filename, tree);
+        load_tree(tree);
+    }
+
+    void TManager::load(const std::string& filename)
+    {
+        clear();
+        add(filename);
+    }
+
+    void TManager::save(const std::string& filename, std::size_t indent_count)
+    {
+        pt::write_xml(filename, make_tree(), std::locale(),
+            pt::xml_writer_make_settings<std::string>(' ', indent_count));
+    }
+
+    void TManager::save_state()
+    {
+        if (!state_cap_) return;
+        std::ostringstream oss;
+        pt::write_xml(oss, make_tree());
+        if (state_cap_ == state_vec_.size())
+        {
+            state_vec_.pop_front();
+        }
+        if (cur_state_ + 1 < state_vec_.size())
+        {
+            state_vec_.erase(
+                std::next(state_vec_.begin(), cur_state_ + 1),
+                state_vec_.end()
+            );
+        }
+        state_vec_.push_back(oss.str());
+        cur_state_ = state_vec_.size() - 1;
+    }
+
+    void TManager::load_state(std::size_t steps, Direction d)
+    {
+        if (!steps || !state_cap_)
+        {
+            return;
+        }
+        else if (d == BACKWARD && steps > cur_state_ ||
+            d == FORWARD && cur_state_ + steps >= state_cap_)
+        {
+            // too far away
+            return;
+        }
+        pt::ptree t;
+        cur_state_ = d == BACKWARD ? 
+            cur_state_ - steps : 
+            cur_state_ + steps;
+        std::istringstream iss(state_vec_[cur_state_]);
+        pt::read_xml(iss, t);
+        clear();
+        load_tree(t);
+    }
+
+    bool SERP::TManager::undo()
+    {
+        if (cur_state_ == 0) 
+            return false;
+        load_state(1, BACKWARD);
+        return true;
+    }
+
+    bool SERP::TManager::redo()
+    {
+        if (cur_state_ == state_vec_.size() - 1)
+            return false;
+        load_state(1, FORWARD);
+        return true;
+    }
+
+    void SERP::TManager::set_state_capacity(std::size_t n)
+    {
+        if (!n)
+        {
+            state_vec_.clear();
+            cur_state_ = 0;
+        }
+        else if (state_cap_ > n)
+        {
+            bool enough_space = state_cap_ - n > state_cap_ - cur_state_ + 1;
+            std::size_t i, j;
+            if (enough_space)
+            {
+                i = state_cap_ - n;
+                j = state_cap_;
+                cur_state_ -= i;
+            }
+            else
+            {
+                i = cur_state_;
+                j = cur_state_ + state_cap_ - n;
+                cur_state_ = 0;
+            }
+            state_vec_.erase(state_vec_.begin(), std::next(state_vec_.begin(), i));
+            state_vec_.erase(std::next(state_vec_.begin(), j), state_vec_.end());
+        }
+        state_cap_ = n;
+    }
+
+    std::size_t TManager::get_state_capacity() const
+    {
+        return state_cap_;
+    }
+
+    void TManager::clear()
+    {
+        divisions_.clear();
+    }
+
+    void TManager::load_tree(const pt::ptree& t)
+    {
+        for (auto& dep : t.get_child("departments"))
         {
             auto div_name = dep.second.get<std::string>("<xmlattr>.name");
             create_division(div_name);
@@ -23,11 +143,11 @@ namespace SERP
                 add_employee(div_name, e);
             }
         }
-	}
+    }
 
-	void TManager::save(const std::string& filename)
-	{
-        pt::ptree tree;
+    pt::ptree TManager::make_tree() const
+    {
+        pt::ptree t;
         for (const Division& dep : divisions_)
         {
             pt::ptree tdep;
@@ -42,9 +162,8 @@ namespace SERP
                 tdep.add_child("employments.employment", edep);
             }
             tdep.add("<xmlattr>.name", dep.name);
-            tree.add_child("departments.department", tdep);
+            t.add_child("departments.department", tdep);
         }
-        pt::write_xml(filename, tree, std::locale(),
-            pt::xml_writer_make_settings<std::string>(' ', 3));
-	}
+        return t;
+    }
 } // namespace SERP
